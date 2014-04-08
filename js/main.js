5c5799fb6e9b16b659da50c8c56874f325f600d1
Greyson Parrelli <greyson.parrelli@gmail.com>
2014-04-08 14:53:30 -0400

    Stores movie data in array in local storage.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 $(document).ready(init);
 
 function init() {
-
     // I wish there was a better way to do this, but Netflix seems to re-use the
     // same content div, and upon hover it queries for the movie info, re-setting
     // the content of the popup
@@ -13,9 +12,34 @@ function init() {
 }
 
 function netfightClick(e) {
+    // Get the reference to the popup box container
     var $parent = $(this).parents('.agMovie');
+
+    // Get all of the movie attributes we want
     var id = $parent.attr('id').substring(3);
+    var title = $parent.find('.title').text().trim();
+    var link = $parent.find('.bobMovieHeader a:first').attr('href');
     var img = $('.vbox_' + id + ' img').attr('src');
+    var obj = {
+        'id': id,
+        'title': title,
+        'link': link,
+        'img': img
+    }
+
+    // Store the movie in local storage
+    chrome.storage.local.get('netfight', function(old) {
+        // Get the old array, or initialize it if it doesn't exist
+        var oldArray = null;
+        if (!old ||  Object.keys(old).length === 0)
+            oldArray = [];
+        else
+            oldArray = old['netfight'];
+
+        // Add our new object to the array and store it
+        oldArray.push(obj);
+        chrome.storage.local.set({ 'netfight': oldArray });
+    });
 }
 
 function ensureButtonExists() {
