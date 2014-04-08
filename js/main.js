326fa5f845551e1b17d0dfb0085bbb995ae9751e
Greyson Parrelli <greyson.parrelli@gmail.com>
2014-04-08 14:32:46 -0400

    Added button that, when clicked, gets the netflix id and the img src.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,29 @@
-alert('hello');
+$(document).ready(init);
+
+function init() {
+
+    // I wish there was a better way to do this, but Netflix seems to re-use the
+    // same content div, and upon hover it queries for the movie info, re-setting
+    // the content of the popup
+    setInterval(ensureButtonExists, 50);
+
+    window.location.onhashchange = function() {
+        console.log('change');
+    }
+}
+
+function netfightClick(e) {
+    var $parent = $(this).parents('.agMovie');
+    var id = $parent.attr('id').substring(3);
+    var img = $('.vbox_' + id + ' img').attr('src');
+}
+
+function ensureButtonExists() {
+    if ($('.bobMovieContent .netfight-btn').length == 0) {
+        $('.bobMovieContent').append($('<button></button>', {
+            'text': 'Netfight',
+            'class': 'netfight-btn'
+        }));
+        $('.bobMovieContent .netfight-btn').click(netfightClick);
+    }
+}
