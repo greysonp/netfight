7850f4e72e94916ce5c276e27106da5037dc7752
MaybeNot <gaboureystockton@gmail.com>
2014-04-13 10:57:31 -0400

    fixed popup images, added netflix rating to netfight array objects

--- js/main.js
+++ js/main.js
@@ -20,11 +20,13 @@ function netfightClick(e) {
     var title = $parent.find('.title').text().trim();
     var link = $parent.find('.bobMovieHeader a:first').attr('href');
     var img = $('.vbox_' + id + ' img').attr('src');
+    var rating = parseInt($parent.find('.sbmfpr').attr('class').split('sbmf-')[1])/10;
     var obj = {
         'id': id,
         'title': title,
         'link': link,
-        'img': img
+        'img': img,
+        'rating': rating
     }
 
     // Store the movie in local storage
